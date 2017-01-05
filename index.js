85ca70e1f9cf426f0694a30b2cec742bb2a49e41
Arnav Gupta <arnav@codingblocks.com>
2017-01-05 15:09:31 +0530

    handle null x-forwarded-for
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- index.js
+++ index.js
@@ -12,14 +12,17 @@ function ExpressGA(uaCode) {
 
     this.middleware = function (req, res, next) {
         let visitor = ua(this.uaCode);
+        if (!req.headers['x-forwarded-for']) {
+            req.headers['x-forwarded-for'] = '0.0.0.0'
+        }
         visitor.pageview({
             dp: req.originalUrl,
             dr: req.get('Referer'),
             ua: req.headers['user-agent'],
-            uip: req.headers['x-forwarded-for'].split(',').pop()
-            || req.connection.remoteAddress
+            uip: req.connection.remoteAddress
             || req.socket.remoteAddress
             || req.connection.socket.remoteAddress
+            || req.headers['x-forwarded-for'].split(',').pop()
         }).send();
         next();
     };
